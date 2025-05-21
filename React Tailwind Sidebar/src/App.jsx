371fd649ff90ea28a367e424751d0192e367f8f8
harishsiri <siripuram.harish8@gmail.com>
2025-05-21 21:55:43 +0530

    initial commit

---
+++ React Tailwind Sidebar/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css'
+import Sidebar from './Sidebar'
+
+function App() {
+
+  return (
+    <>
+      <Sidebar/>
+    </>
+  )
+}
+
+export default App
---
+++ React Tailwind Sidebar/src/Sidebar.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react'
+import { FaHome, FaUserAlt, FaCog, FaSignOutAlt, FaBars } from 'react-icons/fa'
+import {IoCloseSharp} from 'react-icons/io5'
+
+const Sidebar = () => {
+    const[isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className='flex'>
+        {/* sidebar */}
+        <div className={`w-20 md:w-64 bg-gray-800 text-white transition-width duration-300 
+            ${isOpen ? "w-64" : "w-20"}`}> 
+            <div className='flex justify-between items-center p-4'>
+                <h2 className={`text-xl font-bold md:block ${isOpen ? "block" : "hidden"}`}>My App</h2>
+                <button className='block md:hidden' onClick={()=> setIsOpen(!isOpen)}>
+                   {isOpen ? <IoCloseSharp size={24} /> : <FaBars size={24} /> }
+                </button>
+            </div>
+            <nav className='mt-4'>
+                <ul>
+                    <li className='flex items-center p-4 hover:bg-gray-700 cursor-pointer'>
+                        <FaHome size={24} />
+                        <span className={`ml-4 md:block ${isOpen ? "block" : "hidden"}`}>
+                            Home
+                        </span>
+                    </li>
+                    <li className='flex items-center p-4 hover:bg-gray-700 cursor-pointer'>
+                        <FaUserAlt size={24} />
+                        <span className={`ml-4 md:block ${isOpen ? "block" : "hidden"}`}>
+                            Profile
+                        </span>
+                    </li>
+                    <li className='flex items-center p-4 hover:bg-gray-700 cursor-pointer'>
+                        <FaCog size={24} />
+                        <span className={`ml-4 md:block ${isOpen ? "block" : "hidden"}`}>
+                            Settings
+                        </span>
+                    </li>
+                    <li className='flex items-center p-4 hover:bg-gray-700 cursor-pointer'>
+                        <FaSignOutAlt size={24} />
+                        <span className={`ml-4 md:block ${isOpen ? "block" : "hidden"}`}>
+                            Logout
+                        </span>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+        {/* dashbaoard */}
+        <div className='p-8 bg-gray-100 min-h-screen flex-1'> 
+            <h2 className='text-2xl font-bold'>DashBoard</h2>
+            <p className=''>Dashboard to the right side...</p>
+        </div>
+    </div>
+  )
+}
+
+export default Sidebar
